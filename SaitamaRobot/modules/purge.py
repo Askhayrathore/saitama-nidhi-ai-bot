b27c38be0b8d815056fbc1aa7292935813ef9a18
nidhirobot <73631177+nidhirobot@users.noreply.github.com>
2020-11-06 13:49:27 +0530

    Update purge.py

--- SaitamaRobot/modules/purge.py
+++ SaitamaRobot/modules/purge.py
@@ -15,7 +15,7 @@ async def purge_messages(event):
             user_id=event.from_id, message=event) and event.from_id not in [
                 1087968824
             ]:
-        await event.reply("")
+        await event.reply("Only Admins are allowed to use this command")
         return
 
     if not await can_delete_messages(message=event):
